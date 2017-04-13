9e4fc7735797918b24e213d98e56d21664c43127
pa7lux <pa7lux@gmail.com>
2017-04-13 12:43:21 +0300

    deleted website folder

--- website/js/script.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-  $('.mobileMenu').click(function(){
-    $('.mainMobileMenu').animate({'right':'0vw'},'slow');
-  });
-  $('.close').click(function(){
-    $('.mainMobileMenu').animate({'right':'-100vw'},'slow');
-  });
-});
